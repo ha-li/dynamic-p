537574b089af769ebf5bbb994e9da9f10e856be2
ha-li <ha-li@gmail.com>
2016-10-30 14:15:08 -0700

    modify to allow user to pass in the list of coins

--- src/main/java/com/gecko/dynamic/CoinChangeDynamic.java
+++ src/main/java/com/gecko/dynamic/CoinChangeDynamic.java
@@ -5,24 +5,25 @@ package com.gecko.dynamic;
  */
 public class CoinChangeDynamic {
 
-    private static int[] change = {5, 10, 15, 20};
+    //private static int[] change = {5, 10, 15, 20};
     private static int NOT_POSSIBLE = 999;
 
-    public static int max_change(int n) {
+    // modify to allow user to pass in the list of coin_types
+    public static int max_change(int n, int[] coin_types) {
 
         // initialize from 0 ... n this will hold our best change solution
         int[] n_cardinality = n_cardinality = new int [n+1];
         int[] solution = new int[n+1];
 
         // we know that best solution for a value in change[] is 1, so go ahead an initialize that
-        for(int i = 0; i < change.length; i++) {
-            if(change[i] > n) { continue; }
-            n_cardinality[change[i]] = 1;
-            solution[change[i]] = change[i];
+        for(int i = 0; i < coin_types.length; i++) {
+            if(coin_types[i] > n) { continue; }
+            n_cardinality[coin_types[i]] = 1;
+            solution[coin_types[i]] = coin_types[i];
         }
 
         // then we calculate the solution dynamically
-        int retValue = dynamic_min_change(n_cardinality, solution, n);
+        int retValue = dynamic_min_change(n_cardinality, coin_types, solution, n);
         displaySolution(solution, n);
         return retValue;
     }
@@ -38,15 +39,15 @@ public class CoinChangeDynamic {
     }
 
     // returns -1 if change is not possible
-    private static int dynamic_min_change(int[] min_change, int[] solution, int n) {
+    private static int dynamic_min_change(int[] min_change, int[] coin_types, int[] solution, int n) {
 
         // start at i = [1 ... n], make the best change for i using the values in change[]
         for(int i = 1; i <= n; i++) {
             int best = NOT_POSSIBLE;
-            for (int j = 0; j < change.length && change[j] <= i; j++) {
-                if( best > min_change[change[j]] + min_change[i - change[j]] ) {
-                    best = min_change[change[j]] + min_change[i - change[j]];
-                    solution[i] = change[j];// min_change[i - change[j]];
+            for (int j = 0; j < coin_types.length && coin_types[j] <= i; j++) {
+                if( best > min_change[coin_types[j]] + min_change[i - coin_types[j]] ) {
+                    best = min_change[coin_types[j]] + min_change[i - coin_types[j]];
+                    solution[i] = coin_types[j];// min_change[i - change[j]];
                 }
                 //best = min(best, min_change[change[j]] + min_change[i - change[j]]);
             }
--- src/test/java/com/gecko/dynamic/CoinChangeDynamicTest.java
+++ src/test/java/com/gecko/dynamic/CoinChangeDynamicTest.java
@@ -11,17 +11,16 @@ import static org.junit.Assert.*;
 public class CoinChangeDynamicTest {
     @Test
     public void test_change() {
-        int[] solution = new int[5];
-        int best = CoinChangeDynamic.max_change(40);
+        int[] coin_types = {5, 10, 15, 20};
+        int best = CoinChangeDynamic.max_change(40, coin_types);
         System.out.println(best);
         Assert.assertTrue(best == 2);
     }
 
     @Test
     public void test_change_100() {
-        int[] solution = new int[5];
-        int best = CoinChangeDynamic.max_change(10000);
+        int[] coin_types = {5, 10, 15, 25, 100};
+        int best = CoinChangeDynamic.max_change(10000, coin_types);
         System.out.println(best);
-        //Assert.assertTrue(best == 4);
     }
 }
